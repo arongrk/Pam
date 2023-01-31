272ebdcb5b5d01c7b1b2ddf8a7bca36a6e939de9
arongrk <aron.grabski@gmail.com>
2023-01-31 14:38:54 +0100

    tons of modifications, 2 new apps

--- app2.py
+++ app2.py
@@ -5,7 +5,7 @@ from pyqtgraph import *
 from ctypes import c_int32
 import numpy as np
 import random as rnd
-
+import time as t
 
 
 UDP_IP = '192.168.1.1'
@@ -13,7 +13,22 @@ UDP_PORT = 9090
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
+a=0
+time = t.time()
 
 while True:
-    data, addr = sock.recvfrom(10000)
-    print(data)
+    st = t.time()
+    data, addr = sock.recvfrom(1400)
+    # encoded_data = data.decode('utf32-sig')
+    # print(type(data))
+    # print(1)
+    package = np.frombuffer(data, dtype=np.int32)
+    marker = np.where(package == c_int32(0xaaffffff).value)
+    if len(marker[0]) == 0:
+        a += 1
+    else:
+        print(f'marker at position {marker[0]} after {a} packages/ {t.time() - time} seconds')
+        a = 0
+        time = t.time()
+        print(package)
+        break
--- matlabdata.py
+++ matlabdata.py
@@ -2,6 +2,7 @@ import numpy as np
 import random as rnd
 import time as t
 import numpy as np
+import socket
 
 
 def load_adc():
@@ -24,11 +25,14 @@ def load_fpga():
 
 def split_data(array, split_value):
     value = np.where(array == split_value)[0]
-    print(value)
     arrays = np.hsplit(array, value)
     for i in range(len(arrays)):
-        if arrays[i][0] == split_value:
-            arrays[i] = np.delete(arrays[i], 0)
+        try:
+            if arrays[i][0] == split_value:
+                arrays[i] = np.delete(arrays[i], 0)
+        except IndexError:
+            if arrays[1][0] == split_value:
+                arrays[1] = np.delete(arrays[1], 0)
     return arrays
 
 
@@ -42,3 +46,12 @@ def split_350(array, delete_non_complete=True):
 
 def check_length(array, ref_length):
     pass
+
+
+# def socket_receiver(UDP_IP : str, UDP_PORT : int)
+#     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#     sock.bind((UDP_IP, UDP_PORT))
+#     while True:
+#         data, addr = sock.recvfrom(1400)
+#         package = np.frombuffer(data, dtype=np.int32)
+#         yield package
