a8d4f7df743ad96eec85dffe337b2cdb237d466e
arongrk <aron.grabski@gmail.com>
2023-02-13 16:11:31 +0100

    Hacker and byte_sender work now

--- hacker.py
+++ hacker.py
@@ -9,30 +9,25 @@ class Hacker:
 
     def hack(self):
         buffer = bytearray()
-        counter = 0
-        package_len = self.package_len
+        # counter = 0
+        # package_len = self.package_len
         receive = self.receive
         marker = self.marker
         total_len = self.total_len
         marker_len = len(marker)
         while True:
-            data = receive(package_len)
+            data = receive()
             pos = data.find(marker)
-            # your hacker version is actually better than mine because it is simpler
-            # marker is here like a command saying:
-            # 1. copy data up to me into buffer
-            # 2. check buffer len and if correct, yield
-            # 3. reset buffer and append data after me
-            # if pos != -1: # BUG! - if *no* marker found, add data to buffer and continue
             if pos == -1:
                 buffer += data
             else:
                 buffer += data[:pos]
                 if len(buffer) == total_len:
                     yield buffer
+                else:
+                    pass
                 buffer.clear()
                 buffer += data[pos + marker_len:]
-
             # The counter is not really needed anymore as len(buffer) does the job
             # counter += len(data)
             # buffer += data
